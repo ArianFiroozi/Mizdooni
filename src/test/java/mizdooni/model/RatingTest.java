9607321d82ff5723183be8fc955565a507a8f6f3
ArianFiroozi <arianfiroozi3@gmail.com>
2024-10-15 00:21:49 +0330

    add: rating test

--- src/test/java/mizdooni/model/RatingTest.java
+++ src/test/java/mizdooni/model/RatingTest.java
@@ -1,4 +1,13 @@
 package mizdooni.model;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class RatingTest {
+    @Test
+    void OverAllStarCountIsBelowFive() {
+        Rating rating=new Rating();
+        rating.overall=10.5;
+        Assertions.assertTrue(rating.getStarCount()<=5);
+    }
+    // TODO: no negative ratings
 }
