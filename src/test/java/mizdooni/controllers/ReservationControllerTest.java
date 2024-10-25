24210a4bb4e6b3aa98b6ba50c266f8c571d39d2e
ArianFiroozi <arianfiroozi3@gmail.com>
2024-10-25 23:57:33 +0330

    add: some tests for reservation controller

--- src/test/java/mizdooni/controllers/ReservationControllerTest.java
+++ src/test/java/mizdooni/controllers/ReservationControllerTest.java
@@ -1,4 +1,166 @@
 package mizdooni.controllers;
 
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.*;
+
+import mizdooni.model.Reservation;
+import mizdooni.model.Restaurant;
+import mizdooni.response.Response;
+import mizdooni.response.ResponseException;
+import mizdooni.service.ReservationService;
+import mizdooni.service.RestaurantService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Collections;
+import java.util.List;
+
 public class ReservationControllerTest {
+
+    @Mock
+    private RestaurantService restaurantService;
+
+    @Mock
+    private ReservationService reservationService;
+
+    @Mock
+    private Restaurant restaurant;
+
+    @Mock
+    private Reservation reservation;
+
+    @InjectMocks
+    private ReservationController reservationController;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void getReservations_validParams_returnsResponse() {
+        when(restaurantService.getRestaurant(1)).thenReturn(restaurant);
+        Assertions.assertInstanceOf(Response.class, reservationController.getReservations(1, 1, "2023-10-25"));
+    }
+
+    @Test
+    void getReservations_nullRestaurant_throwsException() {
+        when(restaurantService.getRestaurant(1)).thenReturn(null);
+        Assertions.assertThrows(ResponseException.class, ()->{reservationController.getReservations(1, 1, "2023-10-25");});
+    }
+
+    @Test
+    void getReservations_badDate_throwsException() {
+        when(restaurantService.getRestaurant(1)).thenReturn(restaurant);
+        Assertions.assertThrows(ResponseException.class, ()->{reservationController.getReservations(1, 1, "2023-0-25");});
+    }
+
+    @Test
+    void getReservations_reservationServiceThrowsException_throwsException() {
+        when(restaurantService.getRestaurant(1)).thenReturn(null);
+        try {
+            LocalDate date=LocalDate.parse("2023-10-25");
+            when(reservationService.getReservations(1, 1, date)).thenThrow(ResponseException.class);
+        }
+        catch (Exception e) {
+            fail();
+        }
+        Assertions.assertThrows(ResponseException.class, ()->{reservationController.getReservations(1, 1, "2023-10-25");});
+    }
+
+    @Test
+    void getCustomerReservations_validId_returnsResponse() {
+        try {
+            List<Reservation> reservations = Collections.singletonList(reservation);
+            when(reservationService.getCustomerReservations(1)).thenReturn(reservations);
+        }
+        catch (Exception e) {
+            fail();
+        }
+        Assertions.assertInstanceOf(Response.class, reservationController.getCustomerReservations(1));
+    }
+
+    @Test
+    void getCustomerReservations_invalidId_throwsException() {
+        try {
+            when(reservationService.getCustomerReservations(1)).thenThrow(ResponseException.class);
+        }
+        catch (Exception e) {
+            fail();
+        }
+        Assertions.assertThrows(ResponseException.class, ()->{reservationController.getCustomerReservations(1);});
+    }
+
+    @Test
+    void getAvailableTimes_validParams_returnsResponse() {
+        when(restaurantService.getRestaurant(1)).thenReturn(restaurant);
+        LocalDate date=LocalDate.parse("2023-10-25");
+        List<LocalTime> availableTimes = Collections.singletonList(LocalTime.parse("10:01"));
+
+        try {
+            when(reservationService.getAvailableTimes(1, 1, date)).thenReturn(availableTimes);
+        }
+        catch (Exception e) {
+            fail();
+        }
+
+        assertInstanceOf(Response.class, reservationController.getAvailableTimes(1, 1, date.toString()));
+    }
+
+
+    @Test
+    void getAvailableTimes_invalidRestaurant_throwsException() {
+        when(restaurantService.getRestaurant(1)).thenReturn(null);
+        LocalDate date=LocalDate.parse("2023-10-25");
+        List<LocalTime> availableTimes = Collections.singletonList(LocalTime.parse("10:01"));
+
+        try {
+            when(reservationService.getAvailableTimes(1, 1, date)).thenReturn(availableTimes);
+        }
+        catch (Exception e) {
+            fail();
+        }
+
+        Assertions.assertThrows(ResponseException.class, ()->reservationController.getAvailableTimes(1, 1, date.toString()));
+    }
+
+    @Test
+    void getAvailableTimes_invalidDate_throwsException() {
+        when(restaurantService.getRestaurant(1)).thenReturn(restaurant);
+        LocalDate date=LocalDate.parse("2023-12-25");
+        List<LocalTime> availableTimes = Collections.singletonList(LocalTime.parse("10:01"));
+
+        try {
+            when(reservationService.getAvailableTimes(1, 1, date)).thenReturn(availableTimes);
+        }
+        catch (Exception e) {
+            fail();
+        }
+
+        Assertions.assertThrows(ResponseException.class, ()->reservationController.getAvailableTimes(1, 1, "2023-13-25"));
+    }
+
+    @Test
+    void getAvailableTimes_reservationServiceThrowsException_throwsException() {
+        when(restaurantService.getRestaurant(1)).thenReturn(restaurant);
+        LocalDate date=LocalDate.parse("2023-12-25");
+        List<LocalTime> availableTimes = Collections.singletonList(LocalTime.parse("10:01"));
+
+        try {
+            when(reservationService.getAvailableTimes(1, 1, date)).thenThrow(ResponseException.class);
+        }
+        catch (Exception e) {
+            fail();
+        }
+
+        Assertions.assertThrows(ResponseException.class, ()->reservationController.getAvailableTimes(1, 1, date.toString()));
+    }
+
 }
