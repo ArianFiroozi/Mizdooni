581d6e39cdaa912910bfd1ac6300a071a68e6406
ArianFiroozi <arianfiroozi3@gmail.com>
2024-12-26 20:57:24 +0330

    fix: ui test java

--- src/test/java/mizdooni/TestSuite/UITestSuite.java
+++ src/test/java/mizdooni/TestSuite/UITestSuite.java
@@ -1,15 +1,13 @@
-package com.example.tests;
+package mizdooni.TestSuite;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
-public class UntitledTestCase {
+public class UITestSuite {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
@@ -23,7 +21,7 @@ public class UntitledTestCase {
   }
 
   @Test
-  public void testUntitledTestCase() throws Exception {
+  public void testReservationUI() throws Exception {
     driver.get("http://localhost:8080/api/swagger-ui/index.html#/");
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='/signup'])[1]/following::*[name()='svg'][1]")).click();
     driver.findElement(By.xpath("//div[@id='operations-authentication-controller-signup']/div[2]/div/div/div/div[2]/button")).click();
@@ -40,8 +38,8 @@ public class UntitledTestCase {
     driver.findElement(By.xpath("//div[@id='operations-restaurant-controller-getRestaurants']/div/button")).click();
     driver.findElement(By.xpath("//div[@id='operations-restaurant-controller-getRestaurants']/div[2]/div/div/div/div[2]/button")).click();
     driver.findElement(By.xpath("//input[@value='']")).click();
-    driver.findElement(By.xpath("//input[@value='1']")).clear();
-    driver.findElement(By.xpath("//input[@value='1']")).sendKeys("1");
+//    driver.findElement(By.xpath("//input[@value='1']")).clear();
+    driver.findElement(By.xpath("//input[@value='']")).sendKeys("1");
     driver.findElement(By.xpath("//input[@value='1']")).sendKeys(Keys.ENTER);
     driver.findElement(By.xpath("//div[@id='operations-restaurant-controller-getRestaurants']/div[2]/div/div/div[2]/div/table/tbody/tr[2]/td[2]/div/textarea")).click();
     driver.findElement(By.xpath("//div[@id='operations-restaurant-controller-getRestaurants']/div[2]/div/div/div[2]/div/table/tbody/tr[2]/td[2]/div/textarea")).click();
@@ -60,12 +58,12 @@ public class UntitledTestCase {
     driver.findElement(By.xpath("//div[@id='operations-reservation-controller-getAvailableTimes']/div[2]/div/div/div[2]/div/table/tbody/tr/td[2]/input")).sendKeys("1");
     driver.findElement(By.xpath("//div[@id='operations-reservation-controller-getAvailableTimes']/div[2]/div/div/div[2]/div/table/tbody/tr/td[2]/input")).sendKeys(Keys.ENTER);
     driver.findElement(By.xpath("//input[@value='']")).click();
-    driver.findElement(By.xpath("//input[@value='2']")).clear();
-    driver.findElement(By.xpath("//input[@value='2']")).sendKeys("2");
+//    driver.findElement(By.xpath("//input[@value='2']")).clear();
+    driver.findElement(By.xpath("//input[@value='']")).sendKeys("2");
     driver.findElement(By.xpath("//input[@value='2']")).sendKeys(Keys.ENTER);
     driver.findElement(By.xpath("//input[@value='']")).click();
-    driver.findElement(By.xpath("//input[@value='2025-10-26']")).clear();
-    driver.findElement(By.xpath("//input[@value='2025-10-26']")).sendKeys("2025-10-26");
+//    driver.findElement(By.xpath("//input[@value='2025-10-26']")).clear();
+    driver.findElement(By.xpath("//input[@value='']")).sendKeys("2025-10-26");
     driver.findElement(By.xpath("//input[@value='2025-10-26']")).sendKeys(Keys.ENTER);
     driver.findElement(By.xpath("//div[@id='operations-reservation-controller-getAvailableTimes']/div[2]/div/div[2]/button")).click();
     try {
@@ -92,8 +90,8 @@ public class UntitledTestCase {
     driver.findElement(By.xpath("//div[@id='operations-reservation-controller-getCustomerReservations']/div/button")).click();
     driver.findElement(By.xpath("//div[@id='operations-reservation-controller-getCustomerReservations']/div[2]/div/div/div/div[2]/button")).click();
     driver.findElement(By.xpath("//input[@value='']")).click();
-    driver.findElement(By.xpath("//input[@value='4']")).clear();
-    driver.findElement(By.xpath("//input[@value='4']")).sendKeys("4");
+//    driver.findElement(By.xpath("//input[@value='4']")).clear();
+    driver.findElement(By.xpath("//input[@value='']")).sendKeys("4");
     driver.findElement(By.xpath("//input[@value='4']")).sendKeys(Keys.ENTER);
     driver.findElement(By.xpath("//div[@id='operations-reservation-controller-getCustomerReservations']/div[2]/div/div[2]/button")).click();
     try {
@@ -104,8 +102,8 @@ public class UntitledTestCase {
     driver.findElement(By.xpath("//div[@id='operations-reservation-controller-cancelReservation']/div/button")).click();
     driver.findElement(By.xpath("//div[@id='operations-reservation-controller-cancelReservation']/div[2]/div/div/div/div[2]/button")).click();
     driver.findElement(By.xpath("//input[@value='']")).click();
-    driver.findElement(By.xpath("//input[@value='0']")).clear();
-    driver.findElement(By.xpath("//input[@value='0']")).sendKeys("0");
+//    driver.findElement(By.xpath("//input[@value='0']")).clear();
+    driver.findElement(By.xpath("//input[@value='']")).sendKeys("0");
     driver.findElement(By.xpath("//input[@value='0']")).sendKeys(Keys.ENTER);
     driver.findElement(By.xpath("//div[@id='operations-reservation-controller-cancelReservation']/div[2]/div/div[2]/button")).click();
     try {
